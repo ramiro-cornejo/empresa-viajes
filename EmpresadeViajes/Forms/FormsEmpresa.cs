2408d72ac81e2ba95d6e37aecef16864d6c2be33
Ramiro Cornejo <ramiro.s.cornejo@gmail.com>
2022-11-23 21:21:45 -0300

    Incorporando forms

---
+++ EmpresadeViajes/Forms/FormsEmpresa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmpresadeViajes.Forms
+{
+    public partial class FormsEmpresa : Form
+    {
+        public FormsEmpresa()
+        {
+            InitializeComponent();
+        }
+    }
+}
